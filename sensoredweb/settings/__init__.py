cc242891ed73c629a919e85f5457701647e371b6
borand <andrzej@borowiec.ca>
2013-05-31 21:14:22 -0400

    test from lubuntu

--- sensoredweb/settings/__init__.py
+++ sensoredweb/settings/__init__.py
@@ -1,7 +1,7 @@
 from .base import *
 
 try:        
-    from .local import *
+    #from .local import *
     print "Loaded local settings"
 except ImportError:
     print "Import error while attempting to load local.py module"
