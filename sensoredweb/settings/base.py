7dc4128d11077d76aa6dedb9e5b417f781be526c
borand <andrzej@borowiec.ca>
2013-06-26 20:40:42 +0000

    modified allowed hosts

--- sensoredweb/settings/base.py
+++ sensoredweb/settings/base.py
@@ -29,7 +29,7 @@ DATABASES = {
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['sensoredweb.heroku.com']
+ALLOWED_HOSTS = ['*']
 
 #DATABASES = {
 #    'default': dj_database_url.config()
