67b01e8cab5619b5bddb1fa0bf9e6edab00e4773
borand <andrzej@borowiec.ca>
2013-07-11 14:26:08 +0000

    wired up basic djangorestframework functionality

--- sensordata/urls.py
+++ sensordata/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, url
 from sensordata.class_views import ApiDeviceInstanceView, ApiDataValueView, ApiDataValueView
-# from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
-# from . import api_views
+from . import api_views
 
 urlpatterns = patterns('',    
     url(r'^$',views.HomePageView.as_view(), name='home'),
@@ -22,18 +22,18 @@ urlpatterns = patterns('',
     url(r'^api/datavalue/(?P<device>[a-zA-Z0-9-_]+)','sensordata.views.api_get_datavalue', name='data_value_api'),    
     url(r'^api/datavalue/(?P<device>[a-zA-Z0-9-_]+).json?callback=?$','sensordata.views.api_get_datavalue', name='data_value_api'),
 
-    # url(r'^api/units/$', api_views.UnitstList.as_view()),
-    # url(r'^api/units/(?P<pk>[0-9]+)/$', api_views.UnitsDetail.as_view()),
-    # url(r'^api/man/$', api_views.ManufacturerList.as_view()),
+    ## REST-FRAMEWORK-API
+    url(r'^api/units/$', api_views.UnitstList.as_view()),
+    url(r'^api/units/(?P<pk>[0-9]+)/$', api_views.UnitsDetail.as_view()),
+    url(r'^api/man/$', api_views.ManufacturerList.as_view()),
 
-    # url(r'^api/timestamp/$', api_views.TimestampList.as_view()),
-    # url(r'^api/timestamp/(?P<pk>[0-9]+)/$', api_views.TimestampDetail.as_view()),
+    url(r'^api/timestamp/$', api_views.TimestampList.as_view()),
+    url(r'^api/timestamp/(?P<pk>[0-9]+)/$', api_views.TimestampDetail.as_view()),
 
-    # url(r'^rest/datavalue/(?P<device>[a-zA-Z0-9-_]+)/$', api_views.DataValueList.as_view()),
+    url(r'^rest/datavalue/(?P<device>[a-zA-Z0-9-_]+)/$', api_views.DataValueList.as_view()),
 )
 
-
-# urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(urlpatterns)
 
 
 
